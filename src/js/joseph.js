4b7ea55d6b169a434117697c77ccfa04c2a91e64
Andy <andy@xtremeapp.com.tw>
2021-11-19 23:17:31 +0800

    webpack enviroment 設定

--- src/js/joseph.js
+++ src/js/joseph.js
@@ -1,8 +1,10 @@
-import example from '../assets/123.png'
+// import example from '../assets/123.png'
 import '../scss/andy.scss';
 import '../scss/joseph.scss';
 import '../scss/main.scss';
 
 window.onload = () => {
-  console.log('新公司妹子好多好爽喔！')
+  console.log('你好');
+  console.log(process.env.NODE_ENV)
+  console.log(process.env.APP_ENV)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 
-// webpack --env=production  --output-path aaaa
+// --env aaa=zzz 可以透過此方法在 下方 env 中取到 aaa 的值 
+// argv 只能輸入特定的參數才可以取到
 // 透過argv 取得參數  使用  --output-path 改寫輸出的資料夾位置（預設是dist底下）
 
 module.exports = (env, argv) => {
-  console.log('---------')
-  console.log(env)
-  console.log('--=========')
-  console.log(argv)
+
+  const mode = env.mode || 'development';
+
   return ({
-    mode: env.production ? "production" : "development",
+    mode,
     entry: {
-      main: path.resolve(__dirname, './src/js/joseph.js')
+      main: path.resolve(__dirname, './src/js/joseph.js'),
     },
-    output: env.production
+    output: mode === 'production'
     ? {
       path: path.resolve(__dirname, './dist'),
-      filename: '[name].prod.[contenthash:8].js',
+      filename: 'js/[name].prod.[hash].js',
       }
     : {
       path: path.resolve(__dirname, './dist'),
-      filename: '[name].develope.[contenthash:8].js',
+      filename: 'js/[name].dev.[hash].js',
     },
     module: {
       rules: [
@@ -44,35 +45,50 @@ module.exports = (env, argv) => {
           exclude: /node_modules/,
           use: ['babel-loader'],
         },
-        // Images
-        {
-          test: /\.(png|jpg|gif)$/,
-          use: [
-              {
-                  loader: 'file-loader',
-                  options: {
-                      name: '[path][name].[ext]',
-                      context: path.resolve(__dirname, "src/"),
-                      publicPath: '../',
-                      useRelativePaths: true
-                  }
-              }
-          ] 
-        },
+        // Images / file 目前由複製檔案取代(CopyWebpackPlugin)，之後確認是有什麼情匡要用file-loader
+        // {
+        //   test: /\.(png|jpg|gif)$/,
+        //   use: [
+        //       {
+        //           loader: 'file-loader',
+        //           options: {
+        //               name: '[path][name].[ext]',
+        //               context: path.resolve(__dirname, 'src/'),
+        //               publicPath: '../',
+        //               useRelativePaths: true
+        //           }
+        //       }
+        //   ] 
+        // },
   
       ],
+      
     },
 
     plugins: [
+      new CleanWebpackPlugin(),
+      new Dotenv({
+        path: `./.env.${ mode }`,
+      }),
       new HtmlWebpackPlugin({
         title: 'andy joseph',
         template: path.resolve(__dirname, './src/index.html'), // template file
         filename: 'index.html', // output file
       }),
-      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
         filename: '[name]-[contenthash:8].css',
-      })
+      }),
+      // 暫時用來複製檔案 
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'src/assets', to: 'assets' }],
+      }), 
+      // 目前
+      // new webpack.DefinePlugin({
+      //   'process.env':{
+      //     NODE_ENV: JSON.stringify(mode),// 要非boolean 或是numbet 需要 stringify
+      //   } 
+
+      // }),
 
     ],
 
@@ -115,7 +131,7 @@ module.exports = (env, argv) => {
   //               loader: 'file-loader',
   //               options: {
   //                   name: '[path][name].[ext]',
-  //                   context: path.resolve(__dirname, "src/"),
+  //                   context: path.resolve(__dirname, 'src/'),
   //                   outputPath: 'dist/',
   //                   publicPath: '../',
   //                   useRelativePaths: true

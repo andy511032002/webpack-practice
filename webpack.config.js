77585240b3da7fda5ecad1bfb204d13fff47bcba
Joseph Lin <joseph@xtremeapp.com.tw>
2021-12-27 22:10:37 +0800

    adjust mark note

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const svgToMiniDataURI = require("mini-svg-data-uri");
 const Dotenv = require('dotenv-webpack');
 
-// --env aaa=zzz 可以透過此方法在 下方 env 中取到 aaa 的值 
+// --env {parameterName}={parameterValue} 可以透過此方法在 下方 env 中取到 parameterName 的值 
 // argv 只能輸入特定的參數才可以取到
-// 透過argv 取得參數  使用  --output-path 改寫輸出的資料夾位置（預設是dist底下）
+// 透過 argv 取得參數，使用  --output-path 改寫輸出的資料夾位置（預設是dist底下）
 
 module.exports = (env, argv) => {
 
